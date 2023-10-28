33bddb8a7db73c9a15edfcda98af3803d9b456b6
balogun14 <awwalbalogun06@gmail.com>
2023-10-28 23:01:34 +0100

    Added Authentication

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -4,14 +4,13 @@ from .views import (
     TodoCreateView,
     TodoDeleteView,
     TodoUpdateView,
+    LogoutView
     )
 
 urlpatterns = [
-    # path('login',login_form,name='login_form'),
-    # path('signup',signup,name='signup'),
-    # path('logout',logout_form,name='logout'),
     path('del_todo/<int:pk>',TodoDeleteView.as_view(),name='del_todo'),
     path('update_todo/<int:pk>',TodoUpdateView.as_view(),name='update_todo'),
     path('todo_add/',TodoCreateView.as_view(),name='todo_add'),
     path('',HomeListView.as_view(),name='home'),
+    path("logoutpage/", LogoutView.as_view(), name="logoutpage")
 ]
